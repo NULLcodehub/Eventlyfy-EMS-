9160f87e4f937064d961d91bd408b53eca12580f
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2023-08-11 19:18:28 +0600

    new change

---
+++ conn.js
@@ -0,0 +1,195 @@
+var mysql = require('mysql');
+var express=require("express");
+var bodyParser=require('body-parser');
+const session = require('express-session');
+const path=require('path');
+var app=express();
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root03",
+  database:'ems'
+});
+  
+con.connect(function(err) {
+  if (err) throw err;
+  
+}); 
+
+// -----------------------------------------//
+app.set('view engine','ejs');
+
+app.use(express.static(__dirname + '/public'));
+app.get('/register', function(req,res){
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(__dirname+'/public/reg/reg.ejs');
+});
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+// general user regestration
+app.post('/user',function(req,res){
+  var name=req.body.name;
+  var email=req.body.email;
+  var address=req.body.address;
+  var phone=req.body.phone;
+  var password=req.body.password;
+
+  var sql=`Insert into user(name,email,address,phone,password) values("${name}","${email}","${address}","${phone}","${password}")`;
+
+  con.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  })
+
+})
+
+
+
+// eventplaner registration
+
+app.post('/eventplaner',function(req,res){
+  var Cname=req.body.Cname;
+  var email=req.body.email;
+  var address=req.body.address;
+  var phone=req.body.phone;
+  var zip=req.body.zip;
+  var password=req.body.password;
+
+  var sql=`Insert into eventplaner(Cname,email,address,phone,zip,password) values("${Cname}","${email}","${address}","${phone}","${zip}","${password}")`;
+
+  con.query(sql,(err,result)=>{
+    if(err) throw err;
+    res.send(result);
+  })
+
+})
+
+
+
+//login user
+
+app.use(session({ secret: 'cccssseee', resave: false, saveUninitialized: true }));
+
+//routes
+app.get('/', (req, res) => {
+  if (req.session.name) {
+      res.redirect('/home');
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(__dirname+'/public/login/logins.ejs');
+  }
+});
+
+app.get('/home', (req, res) => {
+  if (req.session.name) {
+    // res.setHeader('Content-Type', 'text/html');
+    // res.sendFile(__dirname+'/public/mainpage/homepost.ejs',{name: req.session.name});
+    const templatePath = path.join(__dirname, 'public', 'mainpage', 'homepost.ejs');
+    res.render(templatePath, { name: req.session.name });
+
+
+
+  } else {
+      res.redirect('/');
+  }
+});
+
+// log in verification  
+app.post('/login',(req,res)=>{
+  const {email,password}=req.body;
+  var sql=`SELECT * FROM user where email= ? AND password= ?`;
+  con.query(sql,[email,password],(err,result)=>{
+    if(err) throw err;
+    if(result.length===1){
+      console.log(result[0].name);
+      req.session.name=result[0].name;
+      res.redirect('/home');
+    }else{
+      res.send("invaid userer name and password");
+    }
+  });
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+
+
+// home page
+// app.get('/', function(req,res){
+//   res.setHeader('Content-Type', 'text/html');
+//   res.sendFile(__dirname+'/public/mainpage/homepost.ejs');
+// });
+
+
+
+
+// //login page
+// app.get('/log', function(req,res){
+//   res.setHeader('Content-Type', 'text/html');
+//   res.sendFile(__dirname+'/public/login/logins.ejs');
+// });
+
+
+
+// event planer
+app.get('/log-eventP', function(req,res){
+  const templatePath = path.join(__dirname, 'public', 'eventplaner', 'eventPlaner.ejs');
+  res.render(templatePath);
+
+});
+
+
+//eventprofile
+
+app.get('/eventprofile', (req, res) => {
+  if (req.session.Cname) {
+    const templatePath = path.join(__dirname, 'public', 'eventplaner', 'eventplanerprofile.ejs');
+    res.render(templatePath, { name: req.session.Cname });
+  } else {
+      res.redirect('/log-eventP');
+  }
+});
+
+
+
+
+
+
+app.post('/login-eventP',(req,res)=>{
+  const {Cemail,Cpassword}=req.body;
+  console.log(Cemail);
+  console.log(Cpassword);
+  var sqlp="SELECT * FROM eventplaner where email= ? AND password= ?";
+  con.query(sqlp,[Cemail,Cpassword],(err,result)=>{
+    console.log(result)
+    if(err) throw err;
+    if(result.length===1){
+      console.log(result[0].Cname);
+      req.session.Cname=result[0].Cname;
+      res.redirect('/eventprofile');
+    }else{
+     
+      res.send("invaid userer name and password");
+      
+    }
+  });
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+app.listen(5000);
+
+module.exports=con;
